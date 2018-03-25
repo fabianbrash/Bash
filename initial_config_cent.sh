b35c961fd0c0b6380ec45c27236e5fad683af606
Fabian Brash <fabianbrash@gmail.com>
2018-03-25 17:28:13 -0400

    added sleep logic

--- initial_config_cent.sh
+++ initial_config_cent.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+#Give a warning about running as sudo and make certain all args are inputed
+#I need to programmatically do this in the future
+echo "##########################################################################"
+echo "Please make certain you are running this script with sudo and that all args have been entered on the cli"
+echo "You will have 10 seconds to end this script with CTRL+C"
+echo"##########################################################################"
+
+sleep 10
+
 #Create a new connection for production and take 2 arguments(IP and Gateway)
 nmcli con add type ethernet con-name ens192-prod ifname ens192 ip4 $1/24 gw4 $2 ipv4.dns "8.8.8.8"
 nmcli con up ens192-prod
