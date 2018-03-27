0346fe76f99842125b4693f5293eb380f7ab65f6
Fabian Brash <fabianbrash@gmail.com>
2018-03-27 16:10:57 -0400

    Update initial_config_cent.sh

--- initial_config_cent.sh
+++ initial_config_cent.sh
@@ -10,7 +10,7 @@ echo "##########################################################################
 sleep 10
 
 #Create a new connection for production and take 2 arguments(IP and Gateway)
-nmcli con add type ethernet con-name ens192-prod ifname ens192 ip4 $1/24 gw4 $2 ipv4.dns "10.10.99.161"
+nmcli con add type ethernet con-name ens192-prod ifname ens192 ip4 $1/24 gw4 $2 ipv4.dns "8.8.8.8"
 nmcli con up ens192-prod
 sleep 5
 
@@ -26,14 +26,14 @@ systemd-machine-id-setup
 ##TODO
 
 ###Let's create some temp storage
-mkdir /home/saprime/automated_temp
-cd /home/saprime/automated_temp && curl -LO http://10.10.96.248/sshkeys/keys.pub
+mkdir /home/user/automated_temp
+cd /home/user/automated_temp && curl -LO http://IP/sshkeys/keys.pub
 
 ####Let's setup ssh keys, we know there are 2 users on the system
 #Replace user with the user you want to apply settings to
-if [ ! -d /home/saprime/.ssh ]
+if [ ! -d /home/user/.ssh ]
  then
-     mkdir /home/saprime/.ssh
+     mkdir /home/user/.ssh
 fi
 
 if [ ! -d /root/.ssh ]
@@ -42,19 +42,19 @@ if [ ! -d /root/.ssh ]
 fi
 
 ##Let's create our file and then inject with current keys
-touch /home/saprime/.ssh/authorized_keys
-cat /home/saprime/automated_temp/keys.pub >> /home/saprime/.ssh/authorized_keys
+touch /home/user/.ssh/authorized_keys
+cat /home/user/automated_temp/keys.pub >> /home/user/.ssh/authorized_keys
 
 #Let's copy keys for root as well
 touch /root/.ssh/authorized_keys
-cat /home/saprime/automated_temp/keys.pub >> /root/.ssh/authorized_keys
+cat /home/user/automated_temp/keys.pub >> /root/.ssh/authorized_keys
 
 ##We have to do this because we will be running as root
-chown -R saprime:saprime /home/saprime/.ssh
+chown -R user:user /home/user/.ssh
 
 ###set permission on authorized_keys file
-chmod 700 /home/saprime/.ssh
-chmod 600 /home/saprime/.ssh/authorized_keys
+chmod 700 /home/user/.ssh
+chmod 600 /home/user/.ssh/authorized_keys
 
 #Do for root as well
 chmod 700 /root/.ssh
@@ -62,4 +62,4 @@ chmod 600 /root/.ssh/authorized_keys
 
 
 ###Cleanup time
-rm -rf /home/saprime/automated_temp
+rm -rf /home/user/automated_temp
