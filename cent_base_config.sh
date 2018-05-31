f9e6a6756c3947ce7c98af30171109e31fd35d78
Fabian Brash <fabianbrash@gmail.com>
2018-05-31 14:47:18 -0400

    Initial commit

---
+++ cent_base_config.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+#----------Let's install some basic tools for all our cent images--------------#
+
+#--------Perl is required for VMware guest customization----------------------#
+
+yum update -y && yum install yum-utils perl
