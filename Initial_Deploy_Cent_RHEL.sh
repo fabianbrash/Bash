f773724f2c97f7cc77f3ca6bd85c698bbfd781c5
Fabian Brash <fabianbrash@gmail.com>
2019-02-04 13:58:49 -0500

    added run as root logic

--- Initial_Deploy_Cent_RHEL.sh
+++ Initial_Deploy_Cent_RHEL.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#Make sure we are root
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
 ##Initial config for a minimal CentOS install
 ##Configure our interface, this assumes ens192 is the name
 nmcli con add type ethernet con-name ens192-prod ifname ens192 ip4 $1/24 gw4 $2 ipv4.dns "8.8.8.8"
