e0f53092499932301a9e36b035c4e1d9e7d3d867
Fabian Brash <fabianbrash@gmail.com>
2019-02-09 17:33:47 -0500

    added root check logic

--- initial_config_cent.sh
+++ initial_config_cent.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-#Give a warning about running as sudo and make certain all args are inputed
-#I need to programmatically do this in the future
-echo "##########################################################################"
-echo "Please make certain you are running this script with sudo and that all args have been entered on the cli"
-echo "You will have 10 seconds to end this script with CTRL+C"
-echo "##########################################################################"
 
-sleep 10
+#Make sure we are root
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root"
+   exit 1
+fi
 
 #Create a new connection for production and take 2 arguments(IP and Gateway)
 nmcli con add type ethernet con-name ens192-prod ifname ens192 ip4 $1/24 gw4 $2 ipv4.dns "8.8.8.8"
