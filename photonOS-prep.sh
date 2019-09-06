81b16401de82c2b2c5f9a078d40e63e87267aaae
Fabian Brash <fabianbrash@gmail.com>
2019-09-06 12:12:13 -0400

    Update photonOS-prep.sh

--- photonOS-prep.sh
+++ photonOS-prep.sh
@@ -7,15 +7,15 @@ usermod -G www-data -a www-data
 
 ##Now add a user to the docker group so we don't have to use sudo or use the root account
 useradd $1
-Sleep 3
+sleep 3
 usermod -aG docker $1
 systemctl start docker
 systemctl enable docker
-Sleep 10
+sleep 10
 ###Let's set root to not expire
 
 chage -l root ##Let's see what the expiry currently is
-Sleep 5
+sleep 5
 passwd -x -1 root  ###set to never expire
 
 reboot
