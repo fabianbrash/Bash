72074f6daf5216fc4abd4512e7f487dec9309ee6
Fabian Brash <fabianbrash@gmail.com>
2019-09-06 12:11:46 -0400

    added user logic

--- photonOS-prep.sh
+++ photonOS-prep.sh
@@ -4,5 +4,19 @@ groupadd -g 33 www-data
 #groupadd -g 999 docker
 useradd www-data -u 33
 usermod -G www-data -a www-data
+
+##Now add a user to the docker group so we don't have to use sudo or use the root account
+useradd $1
+Sleep 3
+usermod -aG docker $1
 systemctl start docker
 systemctl enable docker
+Sleep 10
+###Let's set root to not expire
+
+chage -l root ##Let's see what the expiry currently is
+Sleep 5
+passwd -x -1 root  ###set to never expire
+
+reboot
+
