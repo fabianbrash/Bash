ad5d3a10b84eba5c0914d909f5e27924181d1e83
Fabian Brash <fabianbrash@gmail.com>
2020-01-05 11:00:47 -0500

    added root check

--- dhcp-install.sh
+++ dhcp-install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
- yum install -y dhcp-4.2.5
+#Make sure we are root
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+yum install -y dhcp-4.2.5
 
 cp /usr/share/doc/dhcp-4.2.5/dhcpd.conf.example /etc/dhcp/dhcpd.conf
