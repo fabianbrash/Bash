2289e6ab66db4206159e7d2543e5b3b0fb45de4c
Fabian Brash <fabianbrash@gmail.com>
2020-01-05 11:03:31 -0500

    Update dhcp-install.sh

--- dhcp-install.sh
+++ dhcp-install.sh
@@ -10,3 +10,6 @@ fi
 yum install -y dhcp-4.2.5
 
 cp /usr/share/doc/dhcp-4.2.5/dhcpd.conf.example /etc/dhcp/dhcpd.conf
+
+
+echo "Please head on over to /etc/sysconfig/dhcpd and edit the HDCPDARGS variable to the name of your interface ex eth0"
