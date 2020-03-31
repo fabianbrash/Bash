d5991eeb2a717e19f0430e52c898565ddbc1f424
Fabian Brash <fabianbrash@gmail.com>
2020-03-31 08:05:18 -0400

    Update set-hostname.sh

--- set-hostname.sh
+++ set-hostname.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+#Make sure we are root
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+
 ##Set a hostname for the system, this is argument 3
 hostnamectl set-hostname $1 --static
 systemctl restart systemd-hostnamed
