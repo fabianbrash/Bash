42d4bc3c840bd68f3993f7d61e01b487af3bd9fb
Fabian Brash <fabianbrash@gmail.com>
2020-04-05 17:23:45 -0400

    added root check

--- add-k8srepo-debian.sh
+++ add-k8srepo-debian.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+#Make sure we are root
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
