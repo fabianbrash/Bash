2cfffe7537db44e842f427baa17e0173130318fa
Fabian Brash <fabianbrash@gmail.com>
2020-05-20 16:23:33 -0400

    Update docker_fix.sh

--- docker_fix.sh
+++ docker_fix.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-
+yum install -y yum-plugin-versionlock
 yum remove docker-ce -y && yum install -y docker-ce-3:19.03.8-3.el7.x86_64 && systemctl enable --now docker && docker version
+
+yum versionlock docker*
+
