884bad48fa0ff72e39f3f9d7334831b5853c7d4a
Fabian Brash <fabianbrash@gmail.com>
2020-05-20 16:24:44 -0400

    Update docker_fix.sh

--- docker_fix.sh
+++ docker_fix.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#Make sure we are root
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
 yum install -y yum-plugin-versionlock
 yum remove docker-ce -y && yum install -y docker-ce-3:19.03.8-3.el7.x86_64 && systemctl enable --now docker && docker version
 
