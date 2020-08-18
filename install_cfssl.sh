43c0ee6ca7a11063c1ad033c2141c0c5b85adc22
Fabian Brash <fabianbrash@gmail.com>
2020-08-18 11:53:36 -0400

    Update install_cfssl.sh

--- install_cfssl.sh
+++ install_cfssl.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+
+#Make sure we are root
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+#Just in case we don't have it
 yum install -y curl
+
+
 curl -LO https://github.com/cloudflare/cfssl/releases/download/v1.4.1/cfssl_1.4.1_linux_amd64
 curl -LO https://github.com/cloudflare/cfssl/releases/download/v1.4.1/cfssljson_1.4.1_linux_amd64
 
