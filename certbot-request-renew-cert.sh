ddb83e1b1c97a7601026e729e143a70810227c7a
Fabian Brash <fabianbrash@gmail.com>
2020-10-27 15:08:48 -0400

    added TOS and mail

--- certbot-request-renew-cert.sh
+++ certbot-request-renew-cert.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-certbot certonly -d myservice.domain.com --manual --preferred-challenges dns
+certbot certonly -d myservice.domain.com --manual --preferred-challenges dns --agree-tos -m mail@mail.com
