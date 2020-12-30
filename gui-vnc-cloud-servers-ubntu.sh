743eb5973bb77888951213935ad89c60c405a0b9
Fabian Brash <fabianbrash@gmail.com>
2020-12-30 11:22:45 -0500

    added root check

--- gui-vnc-cloud-servers-ubntu.sh
+++ gui-vnc-cloud-servers-ubntu.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#Make sure we are root
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
 apt install -y tightvncserver xfonts-75dpi xfonts-100dpi gsfonts-x11
 
 apt install -y ubuntu-mate-core
