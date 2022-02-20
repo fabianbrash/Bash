092756fcaa4ffcf6155bb6deb7d3cc92731460b8
Fabian Brash <fabianbrash@gmail.com>
2022-02-20 18:14:47 -0500

    Create deb11-bootstrap-containerd-k8s.sh

---
+++ deb11-bootstrap-containerd-k8s.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+apt-get update && apt-get upgrade -y
+
+apt-get install -y apt-transport-https ca-certificates curl
+
+apt-get install -y containerd
+
+cat <<EOF | tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+
+modprobe overlay
+modprobe br_netfilter
+
+# Setup required sysctl params, these persist across reboots.
+cat <<EOF | tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+# Apply sysctl params without reboot
+sysctl --system
+
+mkdir -p /etc/containerd
+containerd config default | tee /etc/containerd/config.toml
+
+systemctl restart containerd
+
+swapoff -a
+
+# Add k8s repos
+curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
