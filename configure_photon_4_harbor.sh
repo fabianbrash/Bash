2c9d609337adafb034f54972da85711134385fea
Fabian Brash <fabianbrash@gmail.com>
2022-05-01 13:09:47 -0400

    Update configure_photon_4_harbor.sh

--- configure_photon_4_harbor.sh
+++ configure_photon_4_harbor.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
-# Patch our system
+# Colors
 
-echo "[TASK 1] checking for updates and patching system"
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+echo -e "${GREEN}[TASK 1] checking for updates and patching system${NC}"
 sleep 2
 tdnf check-update && tdnf upgrade -y
 
-echo "[TASK 2] Installing tar and unzip"
+echo -e "${GREEN}[TASK 2] Installing tar and unzip${NC}"
 sleep 2
 tdnf install -y tar unzip
 
-echo "[TASK 3] Installing Docker-compose"
+echo -e "${GREEN}[TASK 3] Installing Docker-compose${NC}"
 sleep 2
 curl -SL https://github.com/docker/compose/releases/download/v2.4.1/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 
-echo "[TASK 4] Downloading Harbor online installer"
+echo -e "${GREEN}[TASK 4] Downloading Harbor online installer${NC}"
 sleep 3
 mkdir -p /root/harbor-files
 mkdir -p /opt/vmware
@@ -24,28 +27,29 @@ cd /root/harbor-files
 curl -LO https://github.com/goharbor/harbor/releases/download/v2.5.0/harbor-online-installer-v2.5.0.tgz
 tar -xzvf harbor-online-installer-v2.5.0.tgz
 cp -rp /root/harbor-files/harbor /opt/vmware/
+cp -p /opt/vmware/harbor/harbor.yml.tmpl /opt/vmware/harbor/harbor.yml
 
 
-echo "[TASK 5] Fixing network config issues"
+echo -e "${GREEN}[TASK 5] Fixing network config issues${NC}"
 sleep 3
 cp -p /etc/systemd/network/99-dhcp-en.network /etc/systemd/network/99-dhcp-en.network.BAK
 sed -i 's/DHCP=yes/DHCP=no/g' /etc/systemd/network/99-dhcp-en.network
 mv /etc/systemd/network/99-static-en.network /etc/systemd/network/10-static-en.network
 
-echo "[TASK 6] Permit root login over ssh"
+echo -e "${GREEN}[TASK 6] Permit root login over ssh"
 sleep 3
 cp -p /etc/ssh/sshd_config /etc/ssh/sshd_config.ORIG
 sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
 
-echo "[TASK 7] Enabling Docker"
+echo -e "${GREEN}[TASK 7] Enabling Docker${NC}"
 sleep 3
 systemctl enable docker
 
-echo "[TASK 8] Cleaning up"
+echo -e "${GREEN}[TASK 8] Cleaning up${NC}"
 sleep 2
 rm -rf /root/harbor-files
 sleep 3
 
-echo "[TASK 9] REBOOTING..."
+echo -e "${GREEN}[TASK 9] REBOOTING...${NC}"
 sleep 8
 reboot now
