748af6042097ea89431b339c16ab1e9ed4e9c023
Fabian Brash <fabianbrash@gmail.com>
2023-03-24 12:29:16 -0400

    Update certbot-request-renew-cert.sh

--- certbot-request-renew-cert.sh
+++ certbot-request-renew-cert.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-certbot certonly -d myservice.domain.com --manual --preferred-challenges dns --agree-tos -m mail@mail.com
+sudo certbot certonly -d myservice.domain.com --manual --preferred-challenges dns --agree-tos -m mail@mail.com
+
+
+sudo certbot certonly -d "*.mydomain.net" --manual --preferred-challenges dns --agree-tos -m me@me.com
