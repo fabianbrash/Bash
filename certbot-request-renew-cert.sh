56ef5cabadeafe1b9b0b4d567007a0511a8a07fe
Fabian Brash <fabianbrash@gmail.com>
2023-05-24 08:45:53 -0400

    Update certbot-request-renew-cert.sh

--- certbot-request-renew-cert.sh
+++ certbot-request-renew-cert.sh
@@ -4,3 +4,12 @@ sudo certbot certonly -d myservice.domain.com --manual --preferred-challenges dn
 
 
 sudo certbot certonly -d "*.mydomain.net" --manual --preferred-challenges dns --agree-tos -m me@me.com
+
+
+
+# By specifying directories in the users home folder, we don't need root access
+
+certbot certonly -d "*.mydomain.net" --manual --preferred-challenges dns --agree-tos -m me@me.com \
+--logs-dir /home/user/letsencrypt/logs \
+--config-dir /home/user/letsencrypt/config \
+--work-dir /home/user/letsencrypt/work \
