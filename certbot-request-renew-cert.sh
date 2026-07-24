281873d18d3111caa15c89c17c47c1ac16135cf6
Fabian Brash <fabianbrash@gmail.com>
2026-07-24 10:36:03 -0400

    Update certbot-request-renew-cert.sh

--- certbot-request-renew-cert.sh
+++ certbot-request-renew-cert.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+
+DOMAIN="*.mydomain.net"
+LOGDIR="./logs"
+CONFIGDIR="./config"
+WORKDIR="./work"
+EMAIL="me@me.com"
+
 sudo certbot certonly -d myservice.domain.com --manual --preferred-challenges dns --agree-tos -m mail@mail.com
 
 
-sudo certbot certonly -d "*.mydomain.net" --manual --preferred-challenges dns --agree-tos -m me@me.com
+sudo certbot certonly -d ${DOMAIN} --manual --preferred-challenges dns --agree-tos -m me@me.com
 
 
 
 # By specifying directories in the users home folder, we don't need root access
 
-certbot certonly -d "*.mydomain.net" --manual --preferred-challenges dns --agree-tos -m me@me.com \
---logs-dir ./logs \
---config-dir ./config \
---work-dir ./work
+certbot certonly -d ${DOMAIN} --manual --preferred-challenges dns --agree-tos -m ${EMAIL} \
+--logs-dir ${LOGSDIR} \
+--config-dir ${CONFIGDIR} \
+--work-dir ${WORKDIR}
